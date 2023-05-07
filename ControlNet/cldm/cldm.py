caa72c4b0210e75f659611f946ff22c5a192c9f4
Vincent <kmint1819@gmail.com>
2023-05-07 03:46:25 -0500

    temp replace attnblock in controlnet to ours

--- ControlNet/cldm/cldm.py
+++ ControlNet/cldm/cldm.py
@@ -13,7 +13,8 @@ from ldm.modules.diffusionmodules.util import (
 from einops import rearrange, repeat
 from torchvision.utils import make_grid
 from ldm.modules.attention import SpatialTransformer
-from ldm.modules.diffusionmodules.openaimodel import UNetModel, TimestepEmbedSequential, ResBlock, Downsample, AttentionBlock
+from ldm.modules.diffusionmodules.openaimodel import (
+    UNetModel, TimestepEmbedSequential, ResBlock, Downsample, AttentionBlock, OurAttentionBlock)
 from ldm.models.diffusion.ddpm import LatentDiffusion
 from ldm.util import log_txt_as_img, exists, instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
@@ -196,7 +197,7 @@ class ControlNet(nn.Module):
 
                     if not exists(num_attention_blocks) or nr < num_attention_blocks[level]:
                         layers.append(
-                            AttentionBlock(
+                            OurAttentionBlock(
                                 ch,
                                 use_checkpoint=use_checkpoint,
                                 num_heads=num_heads,
@@ -255,7 +256,7 @@ class ControlNet(nn.Module):
                 use_checkpoint=use_checkpoint,
                 use_scale_shift_norm=use_scale_shift_norm,
             ),
-            AttentionBlock(
+            OurAttentionBlock(
                 ch,
                 use_checkpoint=use_checkpoint,
                 num_heads=num_heads,
--- ControlNet/ldm/modules/diffusionmodules/openaimodel.py
+++ ControlNet/ldm/modules/diffusionmodules/openaimodel.py
@@ -26,6 +26,60 @@ def convert_module_to_f16(x):
 def convert_module_to_f32(x):
     pass
 
+import torch
+import torda
+
+class OurAttentionBlock(nn.Module):
+    """
+    An attention block that allows spatial positions to attend to each other.
+    Originally ported from here, but adapted to the N-d case.
+    https://github.com/hojonathanho/diffusion/blob/1e0dceb3b3495bbe19116a5e1b3596cd0706c543/diffusion_tf/models/unet.py#L66.
+    """
+
+    def __init__(
+        self,
+        channels,
+        num_heads=1,
+        num_head_channels=-1,
+    ):
+        super().__init__()
+        self.channels = channels
+        if num_head_channels == -1:
+            self.num_heads = num_heads
+        else:
+            assert (
+                channels % num_head_channels == 0
+            ), f"q,k,v channels {channels} is not divisible by num_head_channels {num_head_channels}"
+            self.num_heads = channels // num_head_channels
+        
+        self.norm = nn.ParameterDict({
+            'weight': nn.Parameter(torch.empty(channels), requires_grad=False),
+            'bias': nn.Parameter(torch.empty(channels), requires_grad=False)
+        })
+        self.qkv = nn.ParameterDict({
+            'weight': nn.Parameter(torch.empty(channels * 3, channels, 1), requires_grad=False),
+            'bias': nn.Parameter(torch.empty(channels * 3), requires_grad=False),
+        })
+        self.proj_out = nn.ParameterDict({
+            'weight': nn.Parameter(torch.empty(channels, channels, 1), requires_grad=False),
+            'bias': nn.Parameter(torch.empty(channels), requires_grad=False),
+        })
+    
+    def load_state_dict(self, state_dict, strict: bool = True):
+        torda.initialize(
+            self.norm.weight,
+            self.norm.bias,
+            self.qkv.weight,
+            self.qkv.bias,
+            self.proj_out.weight,
+            self.proj_out.bias,
+            self.channels,
+            self.num_heads,
+        )
+        return super().load_state_dict(state_dict, strict)
+
+    def forward(self, x):
+        return torda.compute(x, self.channels, self.num_heads)
 
 ## go
 class AttentionPool2d(nn.Module):
@@ -580,7 +634,7 @@ class UNetModel(nn.Module):
 
                     if not exists(num_attention_blocks) or nr < num_attention_blocks[level]:
                         layers.append(
-                            AttentionBlock(
+                            OurAttentionBlock(
                                 ch,
                                 use_checkpoint=use_checkpoint,
                                 num_heads=num_heads,
@@ -637,7 +691,7 @@ class UNetModel(nn.Module):
                 use_checkpoint=use_checkpoint,
                 use_scale_shift_norm=use_scale_shift_norm,
             ),
-            AttentionBlock(
+            OurAttentionBlock(
                 ch,
                 use_checkpoint=use_checkpoint,
                 num_heads=num_heads,
@@ -691,7 +745,7 @@ class UNetModel(nn.Module):
 
                     if not exists(num_attention_blocks) or i < num_attention_blocks[level]:
                         layers.append(
-                            AttentionBlock(
+                            OurAttentionBlock(
                                 ch,
                                 use_checkpoint=use_checkpoint,
                                 num_heads=num_heads_upsample,
